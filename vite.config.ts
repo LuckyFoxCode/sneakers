495830183a848d061e2da2b486692c1445be2b6e
Serhii Shabala <sergei181989@gmail.com>
2023-08-07 17:13:27 +0300

    :recycle: Update configurate files

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       pages: '/src/pages',
+      assets: '/src/assets',
       router: '/src/router',
       components: '/src/components',
       styles: path.resolve(__dirname, './src/scss/styles.scss'),
