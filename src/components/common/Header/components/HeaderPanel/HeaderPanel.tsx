9ad2381a8d838fe7d1fdd366b6cf35479fe22cd7
Serhii Shabala <sergei181989@gmail.com>
2023-08-09 15:27:28 +0300

    :recycle: Refactor HeaderPanel component

--- src/components/common/Header/components/HeaderPanel/HeaderPanel.tsx
+++ src/components/common/Header/components/HeaderPanel/HeaderPanel.tsx
@@ -1,24 +1,19 @@
 import { Icon } from 'components/common';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'router/routes';
 import styles from './HeaderPanel.module.scss';
 
 export const HeaderPanel = () => {
-  const [theme, setTheme] = useState(false);
-
   return (
     <div className={styles.Panel}>
-      <Link
-        to={ROUTES.CART}
-        className={styles.Panel__link}>
+      <button className={styles.Panel__link}>
         <Icon
           name='Cart'
           size={20}
           className={styles.Panel__link_icon}
         />
         <span className={styles.Panel__link_price}>$45</span>
-      </Link>
+      </button>
       <Link
         to={ROUTES.FAVORITES}
         className={styles.Panel__link}>
@@ -37,15 +32,6 @@ export const HeaderPanel = () => {
           className={styles.Panel__link_icon}
         />
       </Link>
-      <button
-        className={styles.Panel__theme}
-        onClick={() => setTheme(!theme)}>
-        <Icon
-          name={theme ? 'Sun' : 'Night'}
-          size={20}
-          className={styles.Panel__theme_icon}
-        />
-      </button>
     </div>
   );
 };
